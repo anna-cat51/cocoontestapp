a270a0d8dafe07b46ac56b53664ed9637707beca
AnnaDoi <willow6510@yahoo.co.jp>
2024-04-11 16:03:32 +0900

    cocoon動作確認おk

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,11 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
 
 import "./src/jquery"
 import "jquery-ui"
+import "@nathanvda/cocoon";
 
-$(function() {
-  console.log("hello world")
-})
+$(function () {
+  console.log("hello world");
+});
