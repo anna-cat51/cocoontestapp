55028e89cf84a98a4092acdc0b3ed38db976963e
AnnaDoi <willow6510@yahoo.co.jp>
2024-04-11 13:25:40 +0900

    jquery導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import "./src/jquery"
+import "jquery-ui"
+
+$(function() {
+  console.log("hello world")
+})
